5cb1bd8bae5e019930d4a0f5d9eeaa6fef7dcf80
RAKIB <rakibislamdev@gmail.com>
2022-01-19 02:59:48 +0600

    calculation section added

--- day1/js/myscript.js
+++ day1/js/myscript.js
@@ -2,7 +2,22 @@
 
 
  
-function myFunction(a, b) {
+function addFunction(a, b) {
     document.getElementById('sum').innerHTML = a+b;
+              
+}
+
+function subFunction(a, b) {
+    document.getElementById('sub').innerHTML = a-b;
+              
+}
+
+function mulFunction(a, b) {
+    document.getElementById('mul').innerHTML = a*b;
+              
+}
+
+function diviFunction(a, b) {
+    document.getElementById('divi').innerHTML = a/b;
               
 }
