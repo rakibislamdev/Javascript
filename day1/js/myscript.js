02a7fa2deb40e1845372fa2dcc79edf3e9fdd6ff
RAKIB <rakibislamdev@gmail.com>
2022-01-20 00:33:35 +0600

    some work added

--- day1/js/myscript.js
+++ day1/js/myscript.js
@@ -21,3 +21,38 @@ function diviFunction(a, b) {
     document.getElementById('divi').innerHTML = a/b;
               
 }
+
+
+/* Variable */
+
+/* using by var */
+var first_name = "Rakib";
+var last_name = "Islam"
+
+console.log(first_name + " " + last_name);
+
+/* using by let */
+let a = "Rakib";
+let b = "Islam"
+
+console.log(a + " " + b);
+
+/* Statement & operator */
+
+let x, y, z;
+x = 5;
+y = 6;
+z = x + y;
+
+console.log(z);
+console.log(x ** 5);
+console.log(y % x);
+console.log(++x);
+
+/* function */
+
+function myFunction(p1, p2) {
+    return p1 * p2;
+  }
+
+  console.log(myFunction(4,5));
