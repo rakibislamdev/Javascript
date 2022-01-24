674c9bc6f8e2140d310e2c5ba1c377102b594c48
RAKIB <rakibislamdev@gmail.com>
2022-01-25 02:19:38 +0600

    searching even and odd number is done

---
+++ class10homework/even_odd.js
@@ -0,0 +1,11 @@
+for(let x = 0; x <= 50; x++){
+    if(x % 2 == 0){
+        document.write(x + ",");
+    }
+}
+
+for(let x = 0; x <= 50; x++){
+    if(x % 2 != 0){
+        document.write(x + "<br>");
+    }
+}
