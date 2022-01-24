37cd623df63c3418531d97980383cd0221259fa8
RAKIB <rakibislamdev@gmail.com>
2022-01-24 13:00:18 +0600

    largest number and for loop without increment

---
+++ class10homework/main.js
@@ -0,0 +1,34 @@
+/*
+let a = prompt("please enter the first number");
+let b = prompt("please enter the second number");
+let c = prompt("please enter the third number");
+let x = Math.max(a,b,c);
+
+console.log(x);
+*/
+
+/* find the largest number between three number */
+var a = 5;
+var b = 8;
+var c = 3;
+
+if(a>b && a>c){
+    console.log("The larges number is " + a);
+}else if(b>a && b>c){
+    console.log("The larges number is " + b);
+}else{
+    console.log("The larges number is " + c);
+}
+
+
+/* for loop without increment and decrement */
+/*
+
+for(i = 0; i < 10;){
+    console.log(i);
+}
+
+*/
+
+/* note: when i run this for loop without increment part the loop is running */
+
